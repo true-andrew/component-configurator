80619a44ad325fef4ca02bb4fb76532d3894fdb7
Andrew <tandrej768@gmail.com>
2022-08-15 14:34:05 +0300

    improved handleEvent -> emit

--- Classes/EventEmitter.js
+++ Classes/EventEmitter.js
@@ -12,18 +12,26 @@ export class EventEmitter {
   }
 
   off(eventName, callback) {
-    this.events[eventName] = this.events[eventName].filter((fn) => callback !== fn);
+    const callbacks = this.events[eventName];
+    const newCallbacks = [];
+    for (let i = 0, len = callbacks.length; i < len; i++) {
+      const targetCallback = callbacks[i];
+      if (targetCallback !== callback) {
+        newCallbacks.push(targetCallback);
+      }
+    }
+    this.events[eventName] = newCallbacks;
   }
 
-  emit(eventName, data) {
-    const event = this.events[eventName];
+  emit(data) {
+    const event = this.events[data.eventName];
     if (event) {
       for (let i = 0, len = event.length; i < len; i++) {
         const object = event[i];
         object.handleEvent(data);
       }
     } else {
-      throw new Error('Unknown event: ' + eventName);
+      throw new Error('Unknown event: ' + data.eventName);
     }
   }
 }
