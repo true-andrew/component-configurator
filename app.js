4ed8940e7efb6892c4b0b27470b9bda2100707e3
Andrew <tandrej768@gmail.com>
2022-08-17 11:41:17 +0300

    added arguments to constructors

--- app.js
+++ app.js
@@ -2,16 +2,10 @@ import {CustomComponent} from "./Classes/CustomComponent.js";
 import {ComponentConfigurator} from "./Classes/Configurator.js";
 import {props1, props2} from "./props.js";
 
-const configurator = new ComponentConfigurator();
+const configurator = new ComponentConfigurator('configurator');
 
-const component1 = new CustomComponent('component1');
-const component2 = new CustomComponent('component2');
-
-// component1.loadProperties(props1);
-// component2.loadProperties(props1);
-
-component1.renderComponent();
-component2.renderComponent();
+const component1 = new CustomComponent('component1', props1);
+const component2 = new CustomComponent('component2', props2);
 
 configurator.register(component1);
 configurator.register(component2);
