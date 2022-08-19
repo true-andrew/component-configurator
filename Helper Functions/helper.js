f5c97e7ae3abd8fb91ca5e8739de583f7895352c
Andrew <tandrej768@gmail.com>
2022-08-19 12:11:45 +0300

    added new functions

--- Helper Functions/helper.js
+++ Helper Functions/helper.js
@@ -1,4 +1,4 @@
-export function createDOMElement(type, className, textContent, dataset) {
+export function createDOMElement(type, textContent, className, dataset) {
   const element = document.createElement(type);
   element.classList.add(className);
   if (dataset !== undefined) {
@@ -12,5 +12,45 @@ export function createDOMElement(type, className, textContent, dataset) {
     element.textContent = textContent;
   }
 
+  return element;
+}
+
+export function createElement(type, textContent) {
+  const element = document.createElement(type);
+  element.textContent = textContent;
+  return element;
+}
+
+/**
+ * @param type = string
+ * @param textContent = string
+ * @param classList = array
+ */
+export function createElementWithClassList(type, textContent, classList) {
+  const element = createElement(type, textContent);
+  for (let i = 0, len = classList.length; i < len; i++) {
+    const className = classList[i];
+    if (className !== '' && className !== undefined) {
+      element.classList.add(className);
+    }
+  }
+  return element;
+}
+
+/**
+ *
+ * @param type = string
+ * @param textContent = string
+ * @param classList = array
+ * @param dataset = object
+ * @returns {*}
+ */
+export function createElementWithClassListAndDataset(type, textContent, classList, dataset) {
+  const element = createElementWithClassList(type, textContent, classList);
+  const keys = Object.keys(dataset);
+  for (let i = 0, len = keys.length; i < len; i++) {
+    const key = keys[i];
+    element.dataset[key] = dataset[key];
+  }
   return element;
 }
